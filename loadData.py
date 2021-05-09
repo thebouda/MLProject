4cdf943c4fb843b2bafcadf89000361c9a7c3cd9
Simone61923 <simo61923@hotmail.com>
2021-05-09 11:09:48 +0200

    insert PCA

--- loadData.py
+++ loadData.py
@@ -6,6 +6,7 @@ Created on Sat May  1 11:57:27 2021
 """
 
 import numpy
+import matplotlib.pyplot as plt
 
 def mcol(v):
     return v.reshape((v.size,1))
@@ -44,7 +45,30 @@ def load(fname):
                 pass
     return numpy.hstack(DList),numpy.array(labelsListQuality,dtype=numpy.int32)
 
+def PCAfunct(D,L):
+    mu = D.mean(1) #media
+    DC = D - mu.reshape((mu.size, 1)) #centrare la matrice
+    DCT = DC.T #trasposta
+    Ctmp=(1/DC.shape[1])*DC
+    C=numpy.dot(Ctmp,DCT) #calcolo la covarianza
+    s, U = numpy.linalg.eigh(C) #autovalori e autovettori
+    P = U[:, ::-1][:, 0:2]
+    DP = numpy.dot(P.T, D)
+    plot_scatter(DP, L)
+    
+def plot_scatter(D,L):  
+    
+    for i in range(2):
+        to_plot = D[:, L==i]
+        plotted = plt.scatter(to_plot[0,:], to_plot[1,:]) # Plot Magic
+        plotted.set_label(int_to_label_quality[i])
+    plt.legend()
+    plt.show()
+    
+
 
 if __name__ == '__main__':
     D,L = load ('./Data/Train.txt')
+    
+    PCAfunct(D,L)
   
